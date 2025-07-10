44ef6115974c9ff44d5f0b45d858c64a21f9739f
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2025-07-10 18:26:26 +0200

    style: unauthorized home view

---
+++ src/stores/movie-quotes-data.js
@@ -0,0 +1,20 @@
+export const MovieQuotesData = [
+    {
+        movie: 'Interstellar',
+        quote: 'You have to leave something behind to go forward',
+        year: 2014,
+        image: 'image1',
+    },
+    {
+        movie: 'The Royal Tenenbaums',
+        quote: 'I think we’re just gonna have to be secretly in love with earch other and leave it that',
+        year: 2001,
+        image: 'image2',
+    },
+    {
+        movie: 'The Lord of the Rings',
+        quote: 'I see in your eyes the same fear that would take the heart of me',
+        year: 2003,
+        image: 'image3',
+    },
+];
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
     <main>
-        <!-- Need to add container -->
         <header class="bg-dark h-[80vh]">
             <BaseContainer>
                 <div class="flex items-center justify-between px-4 py-8">
@@ -26,15 +25,60 @@
                 </div>
             </BaseContainer>
         </header>
+
+        <!-- Movie and Quotes -->
+        <section>
+            <div
+                v-for="(data, index) in movieQuotes"
+                :key="index"
+                class="relative w-[100vw] h-[100vh] bg-center bg-cover"
+                :style="`background-image: url('/src/assets/images/home-page/${data.image}.jpg')`"
+            >
+                <div id="backgroundColor" class="absolute inset-0 z-0"></div>
+
+                <div
+                    class="relative z-10 flex flex-col items-start justify-center text-white w-full max-w-5/6 mx-auto"
+                    :class="{
+                        'pt-80': index === 0,
+                        'pt-100': index === 1,
+                        'pt-135': index === 2,
+                    }"
+                >
+                    <h1 class="font-bold text-5xl w-full max-w-2xl leading-16">
+                        “{{ data.quote }}”
+                    </h1>
+                    <p class="text-gray-200 text-xl mt-4 font-bold">
+                        {{ data.movie }}, <span>{{ data.year }}</span>
+                    </p>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
 <script>
 import BaseButton from '@/components/ui/BaseButton.vue';
 import BaseContainer from '@/components/BaseContainer.vue';
+import { MovieQuotesData } from '@/stores/movie-quotes-data.js';
 
 export default {
     name: 'HomeView',
     components: { BaseContainer, BaseButton },
+
+    data() {
+        return {
+            movieQuotes: [],
+        };
+    },
+
+    created() {
+        this.movieQuotes = MovieQuotesData;
+    },
 };
 </script>
+
+<style>
+#backgroundColor {
+    background-color: rgba(0, 0, 0, 0.35);
+}
+</style>
