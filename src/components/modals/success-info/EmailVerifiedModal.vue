004c9e65855c6cba86c6ebc39dafbcbac290ca53
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2025-07-30 22:40:36 +0200

    feat: verify user with request and show modal

--- src/components/modals/success-info/EmailVerifiedModal.vue
+++ src/components/modals/success-info/EmailVerifiedModal.vue
@@ -16,6 +16,7 @@
 import SuccessMessageInfo from '@/components/ui/SuccessMessageInfo.vue';
 import ModalLayout from '@/components/layouts/ModalLayout.vue';
 import EmailVerifiedIcon from '@/components/icons/modal/EmailVerifiedIcon.vue';
+import { axios } from '@/configs/axios/index.js';
 
 export default {
     name: 'EmailVerifiedModal',
@@ -25,7 +26,37 @@ export default {
         modelValue: Boolean,
     },
 
+    mounted() {
+        if (this.$route.path === '/verify') {
+            const expires = this.$route.query.expires;
+            const userId = this.$route.query.user;
+            const signature = this.$route.query.signature;
+
+            this.verifyUser(expires, userId, signature);
+        }
+    },
+
     methods: {
+        async verifyUser(expires, userId, signature) {
+            console.log(expires, userId, signature);
+            try {
+                const response = await axios.get('/verify', {
+                    params: {
+                        expires: expires,
+                        user: userId,
+                        signature: signature,
+                    },
+                });
+
+                if (response.status === 200) {
+                    this.$emit('update:modelValue', true);
+                }
+            } catch (error) {
+                // TODO: Open linked expired modal
+                console.log(error);
+            }
+        },
+
         closeModal() {
             this.$emit('update:modelValue', !this.modelValue);
         },
